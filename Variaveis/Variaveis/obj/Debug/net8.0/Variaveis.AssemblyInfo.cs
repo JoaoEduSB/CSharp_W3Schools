ad71f8ce55d2bb8fc10337c4761a0e13f4ce5c66
Joao Eduardo <joao.eduardo.souza@hotmail.com>
2025-03-29 23:15:33 -0300

    atualizando arquivos gerais

--- Variaveis/Variaveis/obj/Debug/net8.0/Variaveis.AssemblyInfo.cs
+++ Variaveis/Variaveis/obj/Debug/net8.0/Variaveis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Variaveis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+216b15c3a31193253745228946748c60b9bbbc02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Variaveis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Variaveis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
